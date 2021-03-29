4f32d78c15bb33707603b277378c8052dfa02114
jeremie bitancor <bitancor.jeremie@gmail.com>
2021-03-29 14:14:49 +0800

    Initial Commit

---
+++ H4ck3r News/H4ck3r_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4ck3r_NewsApp.swift
+//  H4ck3r News
+//
+//  Created by jeremie bitancor on 3/29/21.
+//
+
+import SwiftUI
+
+@main
+struct H4ck3r_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
